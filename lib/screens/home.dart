9923adc60b85b8b2d9cb98792c9a0b50f413c76b
Imdvlpr99 <imdvlpr.agency@gmail.com>
2024-01-11 23:46:48 +0700

    feat : implement search controller

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,13 +1,11 @@
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 import 'package:watchflix/api/api_services.dart';
 import 'package:watchflix/utils/constants.dart';
 import 'package:watchflix/utils/custom_dialog.dart';
 import 'package:watchflix/widgets/carousel_item.dart';
 import 'package:watchflix/widgets/home_header.dart';
 import 'package:watchflix/widgets/movie_desc.dart';
-import 'package:watchflix/widgets/search_bar.dart';
 import 'package:watchflix/widgets/section_header.dart';
 import 'package:watchflix/utils/utils.dart';
 import 'package:watchflix/widgets/shimmer_horizontal.dart';
@@ -30,13 +28,26 @@ class _HomeState extends State<Home> {
   List<Movie>? popularList = [];
   List<Genre> movieGenreList = [];
 
-  late int _currentPage  = 0;
+  int _currentPage  = 0;
   Movie? selectedMovie;
+  bool? isSearchEmpty;
 
   @override
   void initState() {
     super.initState();
     fetchData(context);
+    isSearchEmpty = true;
+    _searchController.addListener(() {
+      setState(() {
+        if (_searchController.text.isEmpty) {
+          isSearchEmpty = true;
+          Utils.getLogger().d('isSearchEmpty : $isSearchEmpty');
+        } else {
+          isSearchEmpty = false;
+          Utils.getLogger().d('isSearchEmpty : $isSearchEmpty');
+        }
+      });
+    });
   }
 
   void fetchData(BuildContext context) async {
@@ -69,114 +80,141 @@ class _HomeState extends State<Home> {
   }
 
   @override
+  void dispose() {
+    _searchController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: mainColor,
       body: Column(
         children: [
           Container(
-              margin: EdgeInsets.only(top: MediaQuery.of(context).padding.top),
-              child: HomeHeader(
-                searchController: _searchController,
-              )
+            margin: EdgeInsets.only(top: MediaQuery.of(context).padding.top),
+            child: HomeHeader(
+              searchController: _searchController,
+            ),
           ),
+          isSearchEmpty == true ?
+          Expanded(
+            child: Container(
+              decoration: const BoxDecoration(
+                color: secondaryColor,
+              ),
+              child: Container(
+                decoration: const BoxDecoration(
+                  borderRadius: BorderRadius.only(topRight: Radius.circular(35)),
+                  color: mainColor,
+                ),
+                child: ClipRRect(
+                  borderRadius: const BorderRadius.only(topRight: Radius.circular(35)),
+                  child: SingleChildScrollView(
+                    child: Column(
+                      children: [
+                        const SizedBox(height: 20),
+                        const SectionHeader(
+                          leftTitle: nowPlaying,
+                          rightTitle: seeAll,
+                        ),
+                        const SizedBox(height: 20),
+                        SizedBox(
+                          child: Column(
+                            children: [
+                              CarouselSlider.builder(
+                                itemCount: nowPlayingList?.isEmpty ?? true ? 3 : nowPlayingList!.length,
+                                options: CarouselOptions(
+                                  height: 290,
+                                  viewportFraction: 0.45,
+                                  enableInfiniteScroll: true,
+                                  scrollDirection: Axis.horizontal,
+                                  onPageChanged: (index, reason) {
+                                    setState(() {
+                                      _currentPage = index;
+                                      selectedMovie = nowPlayingList![index];
+                                    });
+                                  },
+                                ),
+                                itemBuilder: (context, index, pageViewIndex) {
+                                  final bool isSelected = _currentPage == index;
+                                  final double scaleFactor = isSelected ? 1.0 : 0.9;
+
+                                  return Transform.scale(
+                                    scale: scaleFactor,
+                                    child: nowPlayingList!.isEmpty
+                                        ? const ShimmerHorizontal()
+                                        : CarouselItem(
+                                      movieItem: nowPlayingList![index],
+                                      isSelected: isSelected,
+                                    ),
+                                  );
+                                },
+                              ),
+                              const SizedBox(height: 5),
+                              if (selectedMovie != null)
+                                MovieDesc(
+                                  movieItem: selectedMovie!,
+                                  genreLists: movieGenreList,
+                                ),
+                            ],
+                          ),
+                        ),
+                        const SizedBox(height: 10),
+                        const SectionHeader(
+                          leftTitle: popular,
+                          rightTitle: seeAll,
+                          rightTitleFontSize: 16,
+                          leftTitleFontSize: 20,
+                        ),
+                        MediaQuery.removePadding(
+                          context: context,
+                          removeTop: true,
+                          child: ListView.separated(
+                            padding: const EdgeInsets.symmetric(vertical: 10),
+                            scrollDirection: Axis.vertical,
+                            shrinkWrap: true,
+                            physics: const NeverScrollableScrollPhysics(),
+                            itemCount: popularList?.isEmpty ?? true ? 3 : popularList!.length,
+                            separatorBuilder: (BuildContext context, int index) {
+                              return const SizedBox(height: 15);
+                            },
+                            itemBuilder: (BuildContext context, int index) {
+                              return popularList!.isEmpty
+                                  ? const ShimmerVertical()
+                                  : MovieVerticalItem(
+                                movieItem: popularList![index],
+                                genreList: movieGenreList,
+                              );
+                            },
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ) :
           Expanded(
               child: Container(
                 decoration: const BoxDecoration(
-                    color: secondaryColor
+                  color: secondaryColor
                 ),
                 child: Container(
-                    decoration: const BoxDecoration(
-                        borderRadius: BorderRadius.only(topRight: Radius.circular(35)),
-                        color: mainColor
-                    ),
-                    child: ClipRRect(
-                      borderRadius: const BorderRadius.only(topRight: Radius.circular(35)),
-                      child: SingleChildScrollView(
-                        child: Column(
-                          children: [
-                            const SizedBox(height: 20),
-                            const SectionHeader(
-                                leftTitle: nowPlaying,
-                                rightTitle: seeAll),
-                            const SizedBox(height: 20),
-                            SizedBox(
-                              child: Column(
-                                children: [
-                                  CarouselSlider.builder(
-                                    itemCount: nowPlayingList?.isEmpty ?? true ? 3 : nowPlayingList!.length,
-                                    options: CarouselOptions(
-                                      height: 290,
-                                      viewportFraction: 0.45,
-                                      enableInfiniteScroll: true,
-                                      scrollDirection: Axis.horizontal,
-                                      onPageChanged: (index, reason) {
-                                        setState(() {
-                                          _currentPage = index;
-                                          selectedMovie = nowPlayingList![index];
-                                        });
-                                      },
-                                    ),
-                                    itemBuilder: (context, index, pageViewIndex) {
-                                      final bool isSelected = _currentPage == index;
-                                      final double scaleFactor = isSelected ? 1.0 : 0.9;
-
-                                      return Transform.scale(
-                                        scale: scaleFactor,
-                                        child: nowPlayingList!.isEmpty
-                                            ? const ShimmerHorizontal()
-                                            : CarouselItem(
-                                          movieItem: nowPlayingList![index],
-                                          isSelected: isSelected,
-                                        ),
-                                      );
-                                    },
-                                  ),
-                                  const SizedBox(height: 5),
-                                  if (selectedMovie != null)
-                                    MovieDesc(
-                                      movieItem: selectedMovie!,
-                                      genreLists: movieGenreList,),
-                                ],
-                              ),
-                            ),
-                            const SizedBox(height: 10),
-                            const SectionHeader(
-                              leftTitle: popular,
-                              rightTitle: seeAll,
-                              rightTitleFontSize: 16,
-                              leftTitleFontSize: 20,
-                            ),
-                            MediaQuery.removePadding(
-                                context: context,
-                                removeTop: true,
-                                child: ListView.separated(
-                                  padding: const EdgeInsets.symmetric(vertical: 10),
-                                  scrollDirection: Axis.vertical,
-                                  shrinkWrap: true,
-                                  physics: const NeverScrollableScrollPhysics(),
-                                  itemCount: popularList?.isEmpty ?? true ? 3 : popularList!.length,
-                                  separatorBuilder: (BuildContext context, int index) {
-                                    return const SizedBox(height: 15);
-                                  },
-                                  itemBuilder: (BuildContext context, int index) {
-                                    return popularList!.isEmpty ? const ShimmerVertical() :
-                                    MovieVerticalItem(
-                                      movieItem: popularList![index],
-                                      genreList: movieGenreList,
-                                    );
-                                  },
-                                )
-                            )
-                          ],
-                        ),
-                      ),
-                    )
+                  decoration: const BoxDecoration(
+                    borderRadius: BorderRadius.only(topRight: Radius.circular(35)),
+                    color: mainColor,
+                  ),
+                  child: ClipRRect(
+                    borderRadius: const BorderRadius.only(topRight: Radius.circular(35)),
+                    child: Container(),
+                  ),
                 ),
               )
-          )
+          ),
         ],
-      )
+      ),
     );
   }
 }
