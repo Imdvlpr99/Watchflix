42f17231d7a7b1b6bdbe12cd851f982c1af2a10d
Imdvlpr99 <imdvlpr.agency@gmail.com>
2024-01-22 22:31:35 +0700

    fix : set text align to center

--- lib/widgets/people_item.dart
+++ lib/widgets/people_item.dart
@@ -16,52 +16,57 @@ class PeopleItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Container(
-          width: 100,
-          height: 150,
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(15)
-          ),
-          child: ClipRRect(
-            borderRadius: BorderRadius.circular(15),
-            child: CachedNetworkImage(
-              imageUrl: '$baseImageUrlOriginal${people.profilePath}',
-              fit: BoxFit.cover,
-              alignment: Alignment.center,
-              width: double.infinity,
-              height: double.infinity,
-              placeholder: (context, url) => Shimmer.fromColors(
-                baseColor: secondaryColor,
-                highlightColor: Colors.grey,
-                child: Container(
-                  decoration: BoxDecoration(
-                    color: secondaryColor,
-                    borderRadius: BorderRadius.circular(15),
+    return SizedBox(
+      width: 100,
+      child: Column(
+        children: [
+          Container(
+            width: 100,
+            height: 150,
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(15)
+            ),
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(15),
+              child: CachedNetworkImage(
+                imageUrl: '$baseImageUrlOriginal${people.profilePath}',
+                fit: BoxFit.cover,
+                alignment: Alignment.center,
+                width: double.infinity,
+                height: double.infinity,
+                placeholder: (context, url) => Shimmer.fromColors(
+                  baseColor: secondaryColor,
+                  highlightColor: Colors.grey,
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: secondaryColor,
+                      borderRadius: BorderRadius.circular(15),
+                    ),
                   ),
                 ),
               ),
             ),
           ),
-        ),
-        const SizedBox(height: 10),
-        Text(
-          people.originalName,
-          style: GoogleFonts.poppins().copyWith(
-            color: white,
-            fontSize: 14,
-            fontWeight: FontWeight.w600
+          const SizedBox(height: 10),
+          Text(
+            people.originalName,
+            textAlign: TextAlign.center,
+            style: GoogleFonts.poppins().copyWith(
+                color: white,
+                fontSize: 14,
+                fontWeight: FontWeight.w600
+            ),
           ),
-        ),
-        Text(
-          people.character,
-          style: GoogleFonts.poppins().copyWith(
-            color: white,
-            fontSize: 10
-          ),
-        )
-      ],
+          Text(
+            people.character,
+            textAlign: TextAlign.center,
+            style: GoogleFonts.poppins().copyWith(
+                color: white,
+                fontSize: 10
+            ),
+          )
+        ],
+      ),
     );
   }
 
